9d9334854062f8c088b1132adbc281896d630057
Panchami P Kumar <117977315+pxnchxm1@users.noreply.github.com>
2024-02-02 21:07:06 +0530

    Lisa's Workbook

---
+++ hr_lisa_workbook.py
@@ -0,0 +1,56 @@
+'''
+LISA's Workbook
+.........................................
+Lisa just got a new math workbook. A workbook contains exercise problems, grouped into chapters. Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located. The format of Lisa's book is as follows:
+
+There are  chapters in Lisa's workbook, numbered from  to .
+The  chapter has  problems, numbered from  to .
+Each page can hold up to  problems. Only a chapter's last page of exercises may contain fewer than  problems.
+Each new chapter starts on a new page, so a page will never contain problems from more than one chapter.
+The page number indexing starts at .'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'workbook' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY arr
+#
+
+def workbook(n, k, arr):
+    special_problem = 0
+    page = 1
+    for i in arr:
+        for prblm in range(1,i+1):
+            if prblm == page:
+                special_problem+=1
+            if i==prblm or prblm%k == 0:
+                page+=1
+    return special_problem
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
