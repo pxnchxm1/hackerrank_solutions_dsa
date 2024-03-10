8be51ec212a556f99d1b8ed594a30522a8873057
Panchami P Kumar <117977315+pxnchxm1@users.noreply.github.com>
2024-03-10 21:02:09 +0530

    InsertionSort2

---
+++ insertionsort2_hr.py
@@ -0,0 +1,40 @@
+'''
+INSERTION - SORT (PART2)
+In Insertion Sort Part 1, you inserted one element into an array at its correct sorted position. Using the same approach repeatedly, can you sort an entire array?
+
+Guideline: You already can place an element into a sorted array. How can you use that code to build up a sorted array, one element at a time? Note that in the first step, when you consider an array with just the first element, it is already sorted since there's nothing to compare it to.
+
+In this challenge, print the array after each iteration of the insertion sort, i.e., whenever the next element has been inserted at its correct position. Since the array composed of just the first element is already sorted, begin printing after placing the second element.
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort2' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort2(n, arr):
+    # Write your code here
+    for i in range(1,len(arr)):
+        for j in range(i):
+            if not (arr[i]>=arr[j]):
+                (arr[i],arr[j]) = (arr[j],arr[i])
+        print(' '.join(map(str, arr)))
+    
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
